d517f29ab0ae2c5305f59415667a7de05b0547b2
Alex <komanyak@yandex.ru>
2021-04-16 19:37:27 +0300

    почти рабочая

--- main.py
+++ main.py
@@ -2,8 +2,38 @@ import sqlite3 as sql
 
 con = sql.connect('test.db')
 
-group_id = input()
-with con:
+
+def add_id(id):
+    group_id = id
+    with con:
+        cur = con.cursor()
+        result = cur.execute("""SELECT * FROM GROUPS""").fetchall()
+        c = 0
+        for i in result:
+            if i[0] == id:
+                c += 1
+        if c == 0:
+            cur.execute(f" INSERT INTO GROUPS VALUES  ('{group_id}')")
+            con.commit()
+
+
+def show(id, day):
+    week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+    day = week[int(day)]
     cur = con.cursor()
-    cur.execute(f"INSERT INTO GROUPS VALUES ('{group_id}')")
+    result = cur.execute("""SELECT {} FROM '{}'  '' """.format(day,id)).fetchall()
+    print(result)
+    return  (result)
+
+def change(id, day, rasp):
+    cur = con.cursor()
+    week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+    day = week[int(day)]
+    rasp = ' '.join(rasp)
+    print(day, id, rasp)
+    cur.execute("""DELETE from '{}' where {} <> ''""".format(id, day))
+    s = """INSERT INTO '{}'('{}') VALUES('{}')""".format(id, day, rasp)
+    cur.execute(s)
     con.commit()
+
+    print(day, id, rasp)
--- vk.py
+++ vk.py
@@ -1,5 +1,7 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from main import add_id, show, change
+from test1 import create_table
 
 vk_session = vk_api.VkApi(
     token='870b2c84cc4b80a88973841f55313ecabe6bb561737bf37726129396955f01512870aaacea5248cf1760a')
@@ -11,21 +13,14 @@ def sender(id, text):
     vk_session.method('messages.send', {'chat_id': id, 'message': text, 'random_id': 0})
 
 
-def add(day, rasp, id):
-    print(day, rasp)
-
-
-def show(id, day):
-    pass
-
-
 def main():
     week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
             if event.from_chat:
-
                 id = event.chat_id
+                add_id(id)
+                create_table()
 
                 msg = event.object.message['text'].lower()
 
@@ -36,22 +31,20 @@ def main():
                     sender(id, 'Введите день недели')
                     f = 'show'
                 if msg in week and f == 'show':
-                    sender(id, 'Расписание на {}:'.format(msg))
+                    sender(id, 'Расписание на {}: {}'.format(msg, show(id, week.index(msg))))
 
-                if msg == 'помощь':
-                    pass
                 if msg == 'изменить расписание':
                     sender(id, 'Введите день недели')
                     f = 'change'
                 if msg in week and f == 'change':
                     sender(id, 'Вводите уроки (каждый с новой строки)')
                     day = msg
-
                     f = 'add'
+
                 if f == 'add':
                     rasp = msg.split()
+                    change(id, week.index(day), rasp)
 
-                    add(day, rasp, id)
 
 
 if __name__ == '__main__':
